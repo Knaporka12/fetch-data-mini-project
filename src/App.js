32676867d5a91bc92d556ff3e4958074ef2d792d
Your Name <you@example.com>
2024-02-03 17:06:54 +0100

    początek, podstawowy wygląd scss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from './Header.js';
+import Nav from './Nav.js';
 
 function App() {
   return (
+
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Header></Header>
+      <Nav></Nav>
+
+      <main className='main'>
+      
+        <h2 className="main__h2">Searching results: </h2>
+
+      </main>
+
     </div>
+
   );
 }
 
---
+++ src/Nav.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+
+    <nav className='nav'>
+    
+        <button className='nav__btn'>Users</button>
+     
+        <button className='nav__btn'>Posts</button>
+     
+        <button className='nav__btn'>Something</button>
+
+    </nav>
+
+  )
+};
+
+export default Nav;
